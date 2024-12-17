5d000ef9af16c8895a263189ae4ff6392b024722
Beatriz Cesconetto <beatriz.cesconetto@compasso.com.br>
2024-12-17 15:21:44 -0300

    first commit

---
+++ src/test/kotlin/br/com/beatriz/ChatgptSpringBootKotlinIntegrationApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.beatriz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatgptSpringBootKotlinIntegrationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
